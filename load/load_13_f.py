1f562ec237db91357cd589f540b9a61379527aa9
pibence <pipis.bence@gmail.com>
2022-09-29 18:06:59 +0200

    loader functions to load 10q filings for traded companies

--- load/load_13_f.py
+++ load/load_13_f.py
@@ -6,12 +6,11 @@ from bs4 import BeautifulSoup
 import re
 
 
-logging.basicConfig(filename='logs/load_13f.log', level=logging.DEBUG, format='%(asctime)s:%(levelname)s:%(message)s')
+logging.basicConfig(filename='logs/load.log', level=logging.DEBUG, format='%(asctime)s:%(levelname)s:%(message)s')
 
 
 def download_13f_filings(name_to_cik:dict, data_folder, filings_folder):
-    
-    dl = Downloader(data_folder)
+    dl = Downloader(f"{data_folder}13f")
     if os.path.exists(filings_folder):
         existing = os.listdir(filings_folder)
 
@@ -66,7 +65,7 @@ def parse_filing(path):
     of a given company and parses the holding into a dataframe. 3 different kind
     of shares can be hold, to obtain the total ownership, the value of 3 stock
     is summarized. The returned df contains the name of the issuer of the stock
-    and the value in USD.
+    and the value in 1000 USD.
     """
 
     soup = BeautifulSoup(open(path, encoding="utf8").read(), "lxml")
@@ -79,10 +78,10 @@ def parse_filing(path):
             # iterating through all companies that are held by the 13f reporter,
             # saving important data
             dic = {}
-            dic["name_of_issuer"] = table.find(re.compile("nameofissuer")).string
-            dic["title_of_class"] = table.find(re.compile("titleofclass")).string
-            dic["cusip"] =table.find(re.compile("cusip")).string
-            dic["value"] = int(table.find(re.compile("value")).string.replace(',', ''))*1000
+            dic["name_of_issuer"] = table.find(re.compile("nameofissuer")).text
+            dic["title_of_class"] = table.find(re.compile("titleofclass")).text
+            dic["cusip"] =table.find(re.compile("cusip")).text
+            dic["value"] = int(table.find(re.compile("value")).text.replace(',', ''))
             records.append(dic)
         
         df =pd.DataFrame(records)
@@ -114,40 +113,49 @@ def create_edgelist_from_df(df, holder):
     return edgelist
 
 
-def parse_filings_to_edgelists(filings_folder, name_to_cik_path, error_csv_path):
+def parse_filings_to_edgelists(filings_folder, name_to_cik_path, error_csv_path, edgelist_path):
     """
     Function that lists all existing 13f reports and parses each of them into
     edgelists, then concatenates the edgelists together. The paths for files that
-    are failed to parse are listed and saved to a csv file.
-    TODO write edgelists to file, divide it into chunks.
+    are failed to parse are listed and saved to a csv file. The edgelists are 
+    written to files in chunks to avoid memory overload.
     """
 
     fils = os.listdir(filings_folder)
     name_to_cik_dict = create_name_to_cik_dict(name_to_cik_path)
+    chunksize = 50
 
     edgelists = []
     failed_paths = []
-    for holder in fils[0:10]:
+    for i, holder in enumerate(fils):
         # get path, name for each 13f reporter company
         path = get_path_for_txt(filings_folder, holder)
         name = name_to_cik_dict[holder]
 
         # parse the txt filing dataframe, if succeeds, parse it to edgelist
         parsed_df = parse_filing(path)
+        
         if parsed_df is not None:
             edgelists.append(create_edgelist_from_df(parsed_df, name))
         else:
             logging.info(f"Could not parse holdings data for {name}")
             failed_paths.append(path)
-            
-    edgelist = pd.concat(edgelists)
-    logging.debug("A run has been finished.")
+
+        # writing the concatenated edgelists to files after a given chunksize to
+        # avoid memory overload
+        if ((i+1) % chunksize == 0)  or ((i+1) == len(fils)):
+            edgelist = pd.concat(edgelists)
+            edgelists = []
+            logging.info(f"{(i+1)/chunksize}. chunk has been written to csv.")
+            edgelist.to_csv(f"{edgelist_path}/{(i+1)/chunksize}_chunk.csv", index=False)
+        
 
     failed_df = pd.DataFrame(failed_paths, columns=["path"])
     failed_df.to_csv(error_csv_path, index=False)
     logging.info(f"paths that failed to parse are written to csv in {error_csv_path}")
+    logging.debug("A run has been finished.")
 
-    return edgelist
+    return 1
 
 
 
--- load/load_reports.py
+++ load/load_reports.py
@@ -4,3 +4,81 @@ from sec_edgar_downloader import Downloader
 import os
 from bs4 import BeautifulSoup
 import re
+
+logging.basicConfig(filename='logs/load.log', level=logging.DEBUG, format='%(asctime)s:%(levelname)s:%(message)s')
+
+
+def get_balace_sheet_data(path):
+    """
+    Function to retrieve total assets, total liabilities and total equity
+    for a given firm from sec 10-Q filing.
+    """
+
+
+
+def download_10q_filings(company_list, data_folder, reports_folder, missing_10q_path):
+    """
+    Function to download 10-q filings from sec edgar database for all given
+    companies. If no 10q filing is available, the name of the company is saved,
+    their value will be calculated with other methods.
+    """
+
+    dl = Downloader(f"{data_folder}10q")
+    comp_wo_10q = []
+
+    if os.path.exists(reports_folder):
+        existing = os.listdir(reports_folder)
+
+    for cname in company_list:
+        if os.path.exists(reports_folder):
+            if cname not in existing:
+                res = download_10q_filing_helper(cname, dl)
+        else:
+            res = download_10q_filing_helper(cname, dl)
+
+        if res ==0:
+            comp_wo_10q.append(cname)
+    failed_df = pd.DataFrame(comp_wo_10q, columns=["not_listed_companies"])
+    failed_df.to_csv(missing_10q_path, index=False)
+    logging.info(f"paths that failed to parse are written to csv in {missing_10q_path}")
+
+    return 1
+
+
+def download_10q_filing_helper(cname, dl):
+    
+    
+    try:
+        res = dl.get("10-Q", cname, amount=1)
+        if res == 1:
+            logging.info(f"Data downloaded for {cname}")
+        if res == 0:
+            logging.info(f"cannot find 10q filing for {cname}")
+        return res
+    except Exception as e:
+        logging.warning(f"when trying to download 10q filings for {cname} the following error occured: {e}")
+
+
+def get_unique_companies(edgelist_path):
+
+    el = load_edgelist(edgelist_path)
+
+    companies = []
+    for col in  ["source", "target"]:
+        companies.extend(el[col].unique())
+    
+    return companies
+    
+
+
+
+def load_edgelist(edgelist_path):
+    edgelists = os.listdir(edgelist_path)
+    
+    el_list = []
+    for edgelist in edgelists:
+        el_list.append(pd.read_csv(f"{edgelist_path}{edgelist}"))
+    
+    df = pd.concat(el_list)
+    
+    return df
