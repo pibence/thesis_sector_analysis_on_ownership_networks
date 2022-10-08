63998a1f5b64e512279881d1e157d91ac68a668b
pibence <pipis.bence@gmail.com>
2022-10-08 17:00:11 +0200

    refactoring similarity calculation and edgelist filtering

--- load/manipulations.py
+++ load/manipulations.py
@@ -3,6 +3,7 @@ import jellyfish
 import itertools
 import logging
 from load.helpers import parse_csvs_from_folder, parse_financials_from_folder
+from tqdm import tqdm
 
 logging.basicConfig(
     filename="logs/load.log",
@@ -12,10 +13,7 @@ logging.basicConfig(
 
 
 def standardize_tickers(column):
-
-    ret_col = ret_col.apply(lambda x: x.split(".")[0])
-
-    return ret_col
+    return column.apply(lambda x: x.split(".")[0])
 
 
 def standardize_names(column):
@@ -71,7 +69,7 @@ def standardize_names(column):
         "pharma",
         "\(.*?\)",
     ]
-    spec_char_dict: {",": "", ".": " ", "&": " ", "/": "", "-": ""}
+    spec_char_dict = {",": "", ".": " ", "&": " ", "/": "", "-": ""}
 
     column = column.astype(str)
     ret_col = column.apply(lambda x: x.lower())
@@ -88,17 +86,14 @@ def standardize_names(column):
     return ret_col
 
 
-def create_node_info_and_filtered_edgelist(
-    financials_folder, edgelist_folder, threshold, node_info_path, edgelist_path
+def create_standardized_edgelist_node_list(
+    financials_folder, edgelist_folder, node_temp_path, edgelist_temp_path
 ):
     """
-    Function to find the tickers and industries for all companies in the edgelist.
-    It also narrows down the scope of companies to the US-based ones by joining
-    the industries file on the edgelist source. For the company names it calculates
-    string similirity measure (Jaro-Winkler) and joins on them above a certain
-    threshold.
+    Function that joins the chunks for the nodes and edges and standardizes their
+    names, then writes them to a temporary folder.
     """
-    
+
     edgelist = parse_csvs_from_folder(edgelist_folder)
     financials = parse_financials_from_folder(financials_folder)
     logging.info("financials data and edgelist chunks are read.")
@@ -109,20 +104,56 @@ def create_node_info_and_filtered_edgelist(
     edgelist["target"] = standardize_names(edgelist["target"])
     logging.info("name standardization finished.")
 
+    financials.to_csv(node_temp_path, index=False)
+    edgelist.to_csv(edgelist_temp_path, index=False)
+    logging.info("Standardized egdelist and node info is written to working directory.")
+
+    return 1
+
+
+def create_similarity_csv(node_temp_path, edgelist_temp_path, sim_path):
+    """
+    Function that calculates the Jaro-Winkler similarity for each possible
+    combinations of the names in two different sources, then writes the similarity
+    dataframe to a csv file.
+    """
+
+    financials = pd.read_csv(node_temp_path)
+    edgelist = pd.read_csv(edgelist_temp_path)
+
     # getting unique values for sources in edgelist
     unique_target = edgelist.groupby("target").count().reset_index()[["target"]]
 
     sim_df = calculate_similarity_df(financials.company_name, unique_target.target)
+    sim_df.to_csv(sim_path, index=False)
     logging.info(
-        "similarity measures are calculated for the names in two different sourcest."
+        f"similarity measures are calculated for the names in two different sources, data is written to {sim_path}"
     )
+
+    return 1
+
+
+def create_node_info_and_filtered_edgelist(
+    sim_path, threshold, node_temp_path, node_path, edgelist_temp_path, edgelist_path
+):
+    """
+    Function that takes the temporary edgelist and node list and similarity
+    file as input, filters the similarity file based on a threshold,
+    changes the names in the edgelists and filters both edgelist and node list
+    to contain only data from the US market that are in both files.
+    """
+
+    sim_df = pd.read_csv(sim_path)
+    financials = pd.read_csv(node_temp_path)
+    edgelist = pd.read_csv(edgelist_temp_path)
+
     filt_sim = sim_df[sim_df.value > threshold]
 
     edgelist = replace_names_in_edgelist(edgelist, filt_sim)
     logging.info("names are substituted in the edgelist")
 
-    # getting unique values for sources in new edgelist
-    unique_source = edgelist.groupby("source").count().reset_index()[["source"]]
+    # getting unique values for targets in new edgelist
+    unique_target = edgelist.groupby("target").count().reset_index()[["target"]]
 
     node_data = financials[financials.company_name.isin(unique_target.target)][
         [
@@ -144,8 +175,8 @@ def create_node_info_and_filtered_edgelist(
         "liabilities",
         "equity",
     ]
-    node_data.to_csv(node_info_path, index=False)
-    logging.info(f"node info csv is written to file at {node_info_path}")
+    node_data.to_csv(node_path, index=False)
+    logging.info(f"node info csv is written to file at {node_path}")
 
     edgelist_filt = edgelist[edgelist.target.isin(node_data.name)]
     edgelist_filt.to_csv(edgelist_path, index=False)
@@ -155,17 +186,20 @@ def create_node_info_and_filtered_edgelist(
 
 
 def calculate_similarity_df(column1, column2):
-    """Function that calculates the string similarity between the two names.
-    Returns a dataframe with similarity measures."""
-    combinations = list(itertools.product(column1, column2))
+    """
+    Function that calculates the string similarity between the two names.
+    Returns a dataframe with similarity measures.
+    """
 
-    sim = []
+    combinations = itertools.product(column1, column2)
+
+    name_list = []
     for com in combinations:
-        sim.append(jellyfish.jaro_winkler_similarity(com[0], com[1]))
+        name_list.append(
+            [com[0], com[1], jellyfish.jaro_winkler_similarity(com[0], com[1])]
+        )
 
-    sim_df = pd.DataFrame(combinations, columns=["description", "source"])
-    sim_df["value"] = sim
-
+    sim_df = pd.DataFrame(name_list, columns=["company_name", "target", "value"])
     return sim_df
 
 
