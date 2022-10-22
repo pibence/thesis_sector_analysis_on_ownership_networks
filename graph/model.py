a134a902cace979fb5546dafbb4dacf7c41c1adf
pibence <pipis.bence@gmail.com>
2022-10-22 18:19:21 +0200

    saving results of simulation

--- graph/model.py
+++ graph/model.py
@@ -4,6 +4,10 @@ import pandas as pd
 from typing import Union
 import logging
 import os
+import csv
+from datetime import datetime
+
+from .describe import get_sector_nodes
 
 
 logging.basicConfig(
@@ -90,13 +94,22 @@ def generate_shock_from_pareto(
 
 def simulate_shocks_from_pareto(
     g: nx.Graph,
-    node_list: Union[list, str],
+    sector: str,
     alpha: float,
     scale: float,
     default_threshold: float,
     repeat: int,
+    simulation_path: str,
+    metadata_path,
 ):
 
+    dir = datetime.now().strftime("%Y_%m_%d_%H%M%S")
+    path = f"{simulation_path}{dir}"
+    os.mkdir(path)
+    logging.debug(
+        f"folder for the current run is created in the simulations folder under the name {dir}"
+    )
+    node_list = get_sector_nodes(g, sector)
     for i in range(0, repeat):
         h = g.copy()
         nx.set_node_attributes(h, None, "default_round")
@@ -109,4 +122,66 @@ def simulate_shocks_from_pareto(
         g_final = propagate_default(g_shocked, default_threshold)
         logging.debug(f"{i+1}. iteration: initial shock is propagated")
 
-    return g_final
+        save_graph_to_feather(g_final, path, i + 1)
+        logging.debug(f"{i+1}. iteration: graph is saved to feather")
+
+    append_simulation_metadata_to_csv(
+        metadata_path,
+        sector,
+        node_list,
+        "pareto",
+        alpha,
+        scale,
+        default_threshold,
+        repeat,
+    )
+    logging.debug(f"metadata is saved for run {dir}")
+
+    return 1
+
+
+def save_graph_to_feather(g, path, iteration):
+
+    df = pd.DataFrame.from_dict(dict(g.nodes(data=True)), orient="index")
+    df = df.reset_index(names="name")
+
+    df.to_feather(f"{path}/{iteration}.feather", compression="zstd")
+
+
+def append_simulation_metadata_to_csv(
+    path,
+    sector,
+    shocked_nodes,
+    shock_dist,
+    alpha,
+    scale_param,
+    default_threshold,
+    no_of_iterations,
+):
+    new_line = [
+        sector,
+        len(shocked_nodes),
+        shock_dist,
+        alpha,
+        scale_param,
+        default_threshold,
+        no_of_iterations,
+    ]
+
+    if os.path.isfile(path):
+        with open(path, "a", newline="") as f:
+            writer = csv.writer(f)
+            writer.writerow(new_line)
+    else:
+        with open(path, "a", newline="") as f:
+            writer = csv.writer(f)
+            header = [
+                "shocked_nodes",
+                "shock_dist",
+                "alpha",
+                "scale_param",
+                "default_threshold",
+                "no_of_iterations",
+            ]
+            writer.writerow(header)
+            writer.writerow(new_line)
